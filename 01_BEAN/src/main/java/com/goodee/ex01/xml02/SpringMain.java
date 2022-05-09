2645e93154524de14e1e2db7d5c8c5c8037c5a8f
jeehui <95328146+jeehui@users.noreply.github.com>
2022-05-09 11:03:05 +0900

    22-05-09

--- 01_BEAN/src/main/java/com/goodee/ex01/xml02/SpringMain.java
+++ 01_BEAN/src/main/java/com/goodee/ex01/xml02/SpringMain.java
@@ -8,7 +8,6 @@ public class SpringMain {
 	public static void main(String[] args) {
 		
 		AbstractApplicationContext ctx = new GenericXmlApplicationContext("xml/context02.xml");
-				
 		Car car = ctx.getBean("bmw_x7", Car.class);
 		
 		car.info(); // 모델, 가격, 엔진 정보 출력하기
